6c4da9a1d45f52536ae2ba33ad13fb224beea988
Szymon Grabowski <szmiko@wp.pl>
2018-08-03 19:21:48 +0200

    Files

---
+++ script.js
@@ -0,0 +1,12 @@
+function Duck(sound) {
+	this.sound = sound;
+	this.quack = function() {
+		console.log(this.sound);
+	}
+}
+
+var toy = new Duck("kwa kwa");
+toy.quack();
+
+console.log(typeof toy);
+console.log(toy instanceof Duck);
