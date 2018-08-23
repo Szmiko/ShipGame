6a46336567ab1b01ba1befa2cf85d075fcf877a8
Szymon Grabowski <szmiko@wp.pl>
2018-08-23 18:40:55 +0200

    Works

--- script.js
+++ script.js
@@ -13,4 +13,47 @@ var view = {
 		var cell = document.getElementById(location);
 		cell.setAttribute("class", "miss");
 	}
-}
+};
+
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+
+	ships: [
+		{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
+		{ locations: ["10", "11", "12"], hits: ["", "", ""] }
+	],
+
+	fire: function(guess) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			
+			var index = ship.locations.indexOf(guess);
+			if (index >= 0) {
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("TRAFIONY!");
+				if  (this.isSunk(ship)) {
+					this.shipsSunk++;
+					view.displayMessage("Zatopiony!");
+				}
+				return true;
+			}
+		}
+	view.displayMiss(guess);
+	view.displayMessage("PUDŁO!");
+	return false;
+	},
+
+	isSunk: function(ship) {
+		for (var i = 0; i < this.shipLength; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false;
+			}
+		}
+	return true;
+	}
+};
