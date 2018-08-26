9297f9eec4e5499c26baa0c013228b3325782727
Szymon Grabowski <szmiko@wp.pl>
2018-08-26 19:04:18 +0200

    End

--- script.js
+++ script.js
@@ -21,11 +21,56 @@ var model = {
 	shipLength: 3,
 	shipsSunk: 0,
 
+	generateShipLocations: function() {
+		var locations;
+		for (var i = 0; i < this.numShips; i++) {
+			do {
+				locations = this.generateShip();
+			} while (this.collision(locations));
+			this.ships[i].locations = locations;
+		}
+	},
+
+	generateShip: function() {
+		var direction = Math.floor(Math.random() * 2);
+		var row, col;
+
+		if (direction === 1) {
+			row = Math.floor(Math.random() * this.boardSize);
+			col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+		} else {
+			row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+			col = Math.floor(Math.random() * this.boardSize);
+		}
+
+		var newShipLocations = [];
+		for (var i = 0; i < this.shipLength; i++) {
+			if (direction === 1) {
+				newShipLocations.push(row + "" + (col + i));
+			} else {
+				newShipLocations.push((row + i) + "" + col);
+			}
+		}
+		return newShipLocations;
+	},
+
 	ships: [
-		{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
-		{ locations: ["10", "11", "12"], hits: ["", "", ""] }
+		{ locations: [0, 0, 0], hits: ["", "", ""] },
+		{ locations: [0, 0, 0], hits: ["", "", ""] },
+		{ locations: [0, 0, 0], hits: ["", "", ""] }
 	],
+
+	collision: function(locations) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = model.ships[i];
+			for (var j = 0; i < this.numShips; i++) {
+				if (ship.locations.indexOf(locations[j]) >= 0) {
+					return true;
+				}
+			}
+		}
+	return false;
+	},
 
 	fire: function(guess) {
 		for (var i = 0; i < this.numShips; i++) {
@@ -57,3 +102,66 @@ var model = {
 	return true;
 	}
 };
+
+var controler = {
+	guesses: 0,
+
+	parseGuess: function(guess) {
+		var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+		if (guess === null || guess.length !== 2) {
+			alert("Nieprawidłowe pole!");
+		} else {
+			firstChar = guess.charAt(0);
+			var row = alphabet.indexOf(firstChar);
+			var column = guess.charAt(1);
+
+			if (isNaN(row) || isNaN(column)) {
+				alert("Nieprawidłowa lokalizacja!");
+			} else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+				alert("Lokalizacja poza planszą!");
+			} else {
+				return row + column;
+			}
+		}
+		return null;
+	},
+
+	processGuess: function(guess) {
+		var location = controler.parseGuess(guess);
+		if (location) {
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips) {
+				view.displayMessage("Wszystkie okręty zatopione! Liczba prób: " + this.guesses);
+			}
+		}
+	}
+};
+
+function init() {
+	var fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handleFireButton;
+	var guessInput = document.getElementById("guessInput");
+	guessInput.onkeypress = handleKeyPress;
+
+	model.generateShipLocations();
+};
+
+function handleKeyPress(e) {
+	var fireButton = document.getElementById("fireButton");
+	if (e.keyCode === 13) {
+		fireButton.click();
+		return false;
+	}
+};
+
+function handleFireButton() {
+	var guessInput = document.getElementById("guessInput");
+	var guess = guessInput.value;
+	controler.processGuess(guess);
+
+	guessInput.value = "";
+};
+
+window.onload = init;
